97a14f14a8b43dd542d906433279878d5ed75985
allen <stuallen6311@gmail.com>
2024-11-16 21:01:33 +0800

    選務資料

--- Taiwan_map/utils.js
+++ Taiwan_map/utils.js
@@ -4,3 +4,19 @@ function getCenterPos() {
 
   return { centerX, centerY };
 }
+
+export function parseCSV(text, id) {
+  const rows = text.split("\n");
+  rows.splice(0, 1);
+  let textRow = rows.join("");
+  textRow = textRow.split('"- ');
+  
+  for (let i = 0; i < textRow.length; i++) {
+    const item = textRow[i].split('",');
+    if(!item[1]) continue
+
+    if (item[1].trim() == id.trim()) {      
+      return { textContent: item[0] };
+    }
+  }
+}
